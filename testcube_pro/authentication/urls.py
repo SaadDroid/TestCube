3da92b59f4930be91ba3d662483329ba6894419a
SaadNoor555 <ssaadn.555@gmail.com>
2022-05-30 11:55:01 +0600

    navigation bar created

--- testcube_pro/authentication/urls.py
+++ testcube_pro/authentication/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from authentication import views
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.login, name="login"),
     path('signup', views.signup, name="signup"),
     path('login', views.login, name="login"),
     path('logout', views.logout, name="logout"),
