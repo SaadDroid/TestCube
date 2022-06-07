a18f6085008164b51ce164dd6d2819e157a4d03e
SaadNoor555 <ssaadn.555@gmail.com>
2022-06-07 11:16:45 +0600

    repositioned some ui elements

--- testcube_pro/DivBy0.java
+++ testcube_pro/DivBy0.java
@@ -1,6 +1,10 @@
 // DivBy0.java
-public class DivBy0{
-public int divBy0(int x){
-return x/(x&1);
-}
+public class DivBy0{
+
+public int divBy0(int x){
+        
+        return x/(x&1);
+
+    }
+
 }
