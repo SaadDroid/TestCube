83bf7acd9856ddd6a38536bf5c116f11442878c5
SaadNoor555 <48786753+SaadNoor555@users.noreply.github.com>
2022-06-10 11:28:35 +0600

    Update settings.py

--- testcube_pro/testcube_pro/settings.py
+++ testcube_pro/testcube_pro/settings.py
@@ -32,8 +32,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bsse1121@iit.du.ac.bd'
-EMAIL_HOST_PASSWORD = 'iit12345'
+EMAIL_HOST_USER = 'email here'
+EMAIL_HOST_PASSWORD = 'password here'
 
 # Application definition
 
