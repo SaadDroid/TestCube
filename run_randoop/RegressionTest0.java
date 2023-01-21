a867c86062d9dbca293d88f23883371361b72780
SaadNoor555 <48786753+SaadNoor555@users.noreply.github.com>
2023-01-22 04:58:05 +0600

    Update RegressionTest0.java

--- run_randoop/RegressionTest0.java
+++ run_randoop/RegressionTest0.java
@@ -2,6 +2,9 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+
+
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class RegressionTest0 {
 
